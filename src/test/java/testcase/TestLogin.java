24ad72710d77c318819e07d067ab380c7fecfdaf
gnavamita <gnavamita@gmail.com>
2025-07-16 21:58:41 +0530

    Adding test cases for login page. I have added 4 tests. JIRA ticket no - https://gnavamita.atlassian.net/browse/SCRUM-29

---
+++ src/test/java/testcase/TestLogin.java
@@ -0,0 +1,36 @@
+package testcase;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestLogin {
+
+
+    @BeforeTest
+    public void openBrowser() {
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+    }
+
+    @Test
+    public void tes1() {
+    }
+
+    @Test
+    public void tes2() {
+    }
+
+
+    @Test
+    public void tes3() {
+    }
+
+    @Test
+    public void tes4() {
+    }
+
+
+}
